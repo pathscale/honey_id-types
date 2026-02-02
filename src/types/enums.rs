f78fc1f9497fabd19c2215bc3f43918c2a9bcca6
dVeon-loch <dveonloch@protonmail.com>
2026-02-02 23:19:25 +0200

    add userstatus enum from auth to exports

--- src/types/enums.rs
+++ src/types/enums.rs
@@ -3,3 +3,4 @@
 pub use crate::types::generated::EnumEndpoint as HoneyEndpointMethodCode;
 pub use crate::types::generated::EnumErrorCode as HoneyErrorCode;
 pub use crate::types::generated::EnumUserRole as HoneyUserRole;
+pub use crate::types::generated::EnumUserStatus as AuthUserStatus;
